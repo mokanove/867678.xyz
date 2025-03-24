98c60d18aa9f28edacec7db3b478172ddba36ffe
sarmioe <oiaiia@867678.xyz>
2025-03-24 11:01:56 +0800

    some

--- src/views/spt.vue
+++ src/views/spt.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h1>Hello! Welcome to Privacidad Mo Kanove's speedtest website</h1>
-        <p>If you browser no anwser , <a href="https://s.867678.xyz/speedtest" style="color: blue;">click here</a></p>
-    </div>
+  <div>
+    <h1>Welcome to Privacidad Mo Kanove's speedtest website</h1>
+    <p>
+      If you browser no anwser ,
+      <a href="https://s.867678.xyz/speedtest" style="color: blue">click here</a>
+    </p>
+  </div>
 </template>
