7594d6b902b73414739e103823d1bcd3d2e01d09
sarmioe <oiaiia@proton.me>
2025-04-19 11:01:36 +0800

    huge update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <el-menu-item index="0" @click="FirP">
       <img style="width: 100px; height: 50px" src="/favicon.ico" alt="SarMioe logo" />
     </el-menu-item>
-    <el-menu-item index="0-0" @click="FirP">Home</el-menu-item>
+    <el-menu-item index="0" @click="FirP">Home</el-menu-item>
     <el-sub-menu index="1">
       <template #title>Social Medias</template>
       <el-menu-item index="1-2" @click="Github">Github</el-menu-item>
@@ -17,24 +17,18 @@
     <el-sub-menu index="2">
       <template #title>My Projects</template>
       <el-menu-item index="2-1" @click="xes">XES</el-menu-item>
-      <el-sub-menu index="2-2">
-        <template #title>tkkhs</template>
-        <el-menu-item index="2-2-1" @click="tkkhsp">Code</el-menu-item>
-        <el-menu-item index="2-2-2" @click="tkkhs">Test in this page</el-menu-item>
+      <el-menu-item index="2-2" @click="tkkhs">tkkhs</el-menu-item>
+      <el-sub-menu index="2-3">
+        <template #title>Code of</template>
+        <el-menu-item index="2-3-1" @click="cfs">this site</el-menu-item>
+        <el-menu-item index="2-3-2" @click="cfsp">speedtest site</el-menu-item>
+        <el-menu-item index="2-3-3" @click="tkkhsp">tkkhs</el-menu-item>
       </el-sub-menu>
-      <el-menu-item index="2-3" @click="cfs">Code of this site</el-menu-item>
       <el-sub-menu index="2-4">
         <template #title>Golang Projects</template>
         <el-menu-item index="2-4-1" @click="MoSIUTER">MoSIUTER</el-menu-item>
         <el-menu-item index="2-4-2" @click="ezgo">ezgo</el-menu-item>
       </el-sub-menu>
-    </el-sub-menu>
-    <el-sub-menu index="3">
-      <template #title>Speedtest</template>
-      <el-menu-item index="3-1" @click="tyo">Tokyo , Japan</el-menu-item>
-      <el-menu-item index="3-2" @click="lacus">Los Angeles , California , US</el-menu-item>
-      <el-menu-item index="3-3" @click="cf">Globle , Cloudflare CDN</el-menu-item>
-      <el-menu-item index="3-4" @click="cfsp">Code of speedtest site</el-menu-item>
     </el-sub-menu>
   </el-menu>
   <router-view></router-view>
@@ -78,16 +72,6 @@ const MoSIUTER = () => {
 }
 const ezgo = () => {
   window.open('https://github.com/sarmioe/ezgo', '_blank')
-}
-const tyo = () => {
-  window.open('https://jp.867678.xyz:82/', '_blank')
-}
-const lacus = () => {
-  window.open('https://us.867678.xyz:82/', '_blank')
-}
-const cf = () => {
-  window.open('https://s.867678.xyz/speedtest', '_blank')
-  router.push('/spt')
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,23 +4,102 @@
       <h1>Hello! Welcome to Privacidad Mo Kanove's website</h1>
       <p>Uh... ago called sarmioe</p>
     </center>
-    <el-card style="max-width: 98%" shandow="hover">
-      <el-button type="info" @click="Github">Github</el-button>
-      <el-divider direction="vertical" />
-      <el-button @click="xes">XES</el-button>
-      <el-button @click="MoSIUTER">MoSIUTER</el-button>
-      <el-button @click="ezgo">ezgo</el-button>
-      <el-button @click="tkkhs">tkkhs</el-button>
+    <el-divider />
+    <el-card style="width: 98%" shandow="hover">
+      <center>
+      <el-button type="warning" @click="Donate">Donate</el-button>
+      <el-button @click="USDT">USDT</el-button>
+      <el-button @click="PP">PayPal</el-button>
       <el-divider direction="vertical" />
       <el-button type="danger" @click="cfsp">Speedtest</el-button>
       <el-button @click="tyo">Tokyo , Japan</el-button>
       <el-button @click="lacus">Los Angeles , California , US</el-button>
       <el-button @click="cf">Global , Cloudflare CDN</el-button>
-      <el-divider direction="vertical" />
-      <el-button type="warning" @click="Donate">Donate</el-button>
-      <el-button @click="USDT">USDT</el-button>
-      <el-button @click="PP">PayPal</el-button>
+    </center>
     </el-card>
+    <el-divider />
+    <h1>Github Projects</h1>
+    <p>Maybe can't be running.</p>
+    <el-row :gutter="20" class="project-section">
+      <el-col :span="8" :xs="24" :sm="12">
+        <el-card class="project-card" shadow="hover">
+          <template #header>
+            <div class="card-header">
+              <span>XES Operating System</span>
+            </div>
+          </template>
+          <p>
+            It's an operating system in development.<br>
+            <img
+              src="https://raw.githubusercontent.com/sarmioe/xes/main/icon/xes%20logo.png"
+              alt="XES Logo"
+              class="project-img"
+            />
+          </p>
+          <template #footer>
+            <el-button @click="xes">Link to Github</el-button>
+          </template>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8" :xs="24" :sm="12">
+        <el-card class="project-card" shadow="hover">
+          <template #header>
+            <div class="card-header">
+              <span>MoSIUTER</span>
+            </div>
+          </template>
+          <p>
+            A modern encryption protocol.<br>
+            This protocol uses multi-mode obfuscation + encryption to transmit your data, ensuring that even if others get your data, they don't know your true intentions, and disguise it like a normal Internet request.<br>
+            Both the transmission protocol and encryption features are specified by an IV, and this IV changes every few minutes to ensure that the data packet cannot be judged by the DPI rules.<br>
+            First, your data will be optimized, then encrypted with snowflake or obfs4 to remove features, and then encrypted with TLS, so that the data packet looks like normal Internet encrypted data.
+          </p>
+          <template #footer>
+            <el-button @click="MoSIUTER">Link to Github</el-button>
+          </template>
+        </el-card>
+      </el-col>
+
+      <el-col :span="8" :xs="24" :sm="12">
+        <el-card class="project-card" shadow="hover">
+          <template #header>
+            <div class="card-header">
+              <span>ezgo</span>
+            </div>
+          </template>
+          <p>
+            This tool makes Go and Git easy to use. Just do it like this: ezgo -f to directly format the Go language code.<br><br>
+            Compared with Git and Go, which require remembering complex commands and a lot of interactions, ezgoit makes them simpler.<br>
+            ezgoit comes from: Easy+Go+Git. When it is actually running, it is simplified to ezgo for easy operation.<br>
+            Open source tool, conditionally open source based on GPLv3 license.
+          </p>
+          <template #footer>
+            <el-button @click="ezgo">Link to Github</el-button>
+          </template>
+        </el-card>
+      </el-col>
+      <el-col :span="8" :xs="24" :sm="12">
+        <el-card class="project-card" shadow="hover">
+          <template #header>
+            <div class="card-header">
+              <span>tkkhs</span>
+            </div>
+          </template>
+          <p>
+            This page will not record anything. If you have something you want to say but are afraid others will know, feel free to say it.<br>
+            If you too afraid , Please download the index.html file and run in your computer.<br>
+            Only you know that , Right?<br>
+          </p>
+          <template #footer>
+            <el-button @click="tkkhsp">Link to Github</el-button>
+            <el-button @click="tkkhs">Test at this page</el-button>
+          </template>
+        </el-card>
+      </el-col>
+    </el-row>
+  <el-divider />
+
     <el-divider />
     <p>So just this , no more.</p>
   </div>
@@ -45,11 +124,11 @@ const ezgo = () => {
 const tkkhs = () => {
   router.push('/tkkhs')
 }
+const tkkhsp = () => {
+  window.open('https://github.com/sarmioe/tkkhs', '_blank')
+}
 const cfsp = () => {
   window.open('https://github.com/sarmioe/servers', '_blank')
-}
-const Github = () => {
-  window.open('https://github.com/sarmioe', '_blank')
 }
 const tyo = () => {
   window.open('https://jp.867678.xyz:82/', '_blank')
@@ -63,8 +142,8 @@ const cf = () => {
 }
 const Donate = () => {
   ElNotification({
-    title: 'Uh...',
-    message: "I didn't add any functionality to this button.",
+    title: 'Thanks!',
+    message: "But this button can't be donate for me , please click USDT or Paypal button to donate.",
     type: 'info',
   })
 }
@@ -88,3 +167,36 @@ const PP = () => {
   window.open('https://www.paypal.com/paypalme/mpjysk?country.x=C2&locale.x=zh_XC', '_blank')
 }
 </script>
+<style>
+.container {
+  padding: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.welcome-section {
+  margin-bottom: 40px;
+}
+.welcome-card {
+  text-align: center;
+}
+.project-section {
+  margin-bottom: 40px;
+}
+.project-card {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  transition: transform 0.3s;
+}
+.project-card:hover {
+  transform: translateY(-5px);
+}
+.project-img {
+  max-width: 100%;
+  height: auto;
+  margin-top: 10px;
+}
+.card-header {
+  font-weight: bold;
+}
+</style>
