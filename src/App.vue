64f948d02197d305d0b7d25419aa0ac80350f97b
mokanove <mpjy000@gmail.com>
2025-07-26 15:41:45 +0800

    California Love!

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,51 @@
     <el-menu-item index="0" @click="FirP">Home</el-menu-item>
     <el-menu-item index="1" @click="hp">867678.xyz</el-menu-item>
   </el-menu>
-  <div>
+<div>
     <h1>Welcome to Mo Kanove's speedtest website</h1>
     <p>
-      If you browser no anwser ,<a href="./speedtest" style="color: blue">click here</a>
+      If you browser no anwser ,<a href="https://s.867678.xyz/speedtest" style="color: blue">click here</a>
     </p>
-  </div>
+  <el-card style="max-width: 98%">
+    <template #header>
+      <div class="card-header">
+        <span>File information</span>
+        <br>
+        <p></p>
+      </div>
+    </template>
+    <p>Creat using : dd if=/dev/zero of=speedtest bs=1M count=300</p>
+    <p>File size: 300MB</p>
+    <p>MD5:0d97a9cd8bbd7ce75a2a76bb06258915</p>
+    <p>SHA256:17a88af83717f68b8bd97873ffcf022c8aed703416fe9b08e0fa9e3287692bf0</p>
+    <p>SHA512:6a7b4118945e8ef03063cbe92e1acfe0144520a8eb0bf32735ffc40dc73faf23f2a16c51a48bc91853a74d0999a1497c7ce1e7ac61b07bd67b6c426249285e0c</p>
+    <p></p>
+  </el-card>
+  <el-divider />
+  <el-card style="max-width: 98%">
+    <template #header>
+      <div class="card-header">
+        <span>Server information</span>
+        <br>
+        <p>Info from ipinfo.io and I recommend you to use ipinfo, their data is very reliable.</p>
+      </div>
+    </template>
+    <p>I admit that I'm stuck here, so I'll write this down first and solve it later</p>
+    <p></p>
+  </el-card>
+</div>
+
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
 
-const router = useRouter()
 const activeIndex = ref('0')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 const FirP = () => {
-  router.push('/')
+  window.open('')
 }
 const hp = () => {
   window.open('https://867678.xyz', '_blank')
