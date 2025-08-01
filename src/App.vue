9a5fec43e52c811f747a2bcfcf33fb7ef9561b2d
mokanove <mpjy000@gmail.com>
2025-08-01 13:20:49 +0800

    you 100% love this change

--- src/App.vue
+++ src/App.vue
@@ -13,39 +13,33 @@
     <el-menu-item index="1" @click="hp">867678.xyz</el-menu-item>
   </el-menu>
 <div>
-    <h1>Welcome to Mo Kanove's speedtest website</h1>
+    <h1>Thank You!</h1>
     <p>
-      If you browser no anwser ,<a href="https://s.867678.xyz/speedtest" style="color: blue">click here</a>
+      If you browser no anwser ,<a href="./speedtest" style="color: blue">click here</a>
     </p>
-  <el-card style="max-width: 98%">
-    <template #header>
-      <div class="card-header">
-        <span>File information</span>
-        <br>
-        <p></p>
-      </div>
-    </template>
-    <p>Creat using : dd if=/dev/zero of=speedtest bs=1M count=300</p>
-    <p>File size: 300MB</p>
-    <p>MD5:0d97a9cd8bbd7ce75a2a76bb06258915</p>
-    <p>SHA256:17a88af83717f68b8bd97873ffcf022c8aed703416fe9b08e0fa9e3287692bf0</p>
-    <p>SHA512:6a7b4118945e8ef03063cbe92e1acfe0144520a8eb0bf32735ffc40dc73faf23f2a16c51a48bc91853a74d0999a1497c7ce1e7ac61b07bd67b6c426249285e0c</p>
-    <p></p>
+    <el-card style="width: 100%" shadow="hover">
+    <template #header>File information</template>
+    <p>Creat using : dd if=/dev/zero of=speedtest bs=1M count=286</p>
+    <p>File size: 286MiB(300,000,000Bytes)</p>
+    <p>MD5:4baf99888b333a7330f5c97c17e5a9df</p>
+    <p>SHA1:7779624bf383486e52118a1a6157bd282795f29b</p>
+    <p>SHA256:e8671610daa5dc152578d9bfe8e25346aa73fa600f908b235f55bf51d0eb5a05</p>
+    <p>SHA512:4b5dafa675246d1393d620c717af4ef7a3b0041ae87e3553d7e00e5b99d3cb065a21a467299b403930c06afa4d8fb069bfd412eed7e9b81d0bd81dcd63d2a90c</p>
   </el-card>
   <el-divider />
-  <el-card style="max-width: 98%">
-    <template #header>
-      <div class="card-header">
-        <span>Server information</span>
-        <br>
-        <p>Info from ipinfo.io and I recommend you to use ipinfo, their data is very reliable.</p>
-      </div>
-    </template>
-    <p>I admit that I'm stuck here, so I'll write this down first and solve it later</p>
-    <p></p>
+  <el-card style="width: 100%" shadow="hover">
+    <template #header>Server information</template>
+    <p>Domain:https://s.867678.xyz/speedtest</p>
+    <p>Bucket using Cloudflare R2(EU) and Cloudflare CDN.</p>
+    <img
+        src="https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/456430b7-1c8f-42b0-71c0-586ad9172700/public"
+        height="300px"
+    />
   </el-card>
 </div>
+<div>
 
+</div>
 </template>
 
 <script lang="ts" setup>
