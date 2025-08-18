88a15e335a04e679ca024476ee44b64557247e3b
MoKanove <mpjy000@gmail.com>
2025-08-18 15:24:56 +0800

    github you sun of a bitch

--- src/App.vue
+++ src/App.vue
@@ -9,37 +9,80 @@
       <el-menu-item index="1-2" @click="pa">867678.xyz</el-menu-item>
     </el-sub-menu>
   </el-menu>
-  <h1>Thank You!</h1>
-    <p>
-      If you browser no anwser ,<a href="./speedtest" style="color: blue">click here</a>
-    </p>
+  <h1>Speed test website by MoKanove.</h1>
+  <h2>HTTP(S)</h2>
+  <p>Please select the file size manually and the download will start automatically.</p>
     <el-card style="width: 100%" shadow="hover">
-    <template #header>File information</template>
-    <p>Creat using : dd if=/dev/zero of=speedtest bs=1M count=286</p>
-    <p>File size: 286MiB(300,000,000Bytes)</p>
-    <p>MD5:4baf99888b333a7330f5c97c17e5a9df</p>
-    <p>SHA1:7779624bf383486e52118a1a6157bd282795f29b</p>
-    <p>SHA256:e8671610daa5dc152578d9bfe8e25346aa73fa600f908b235f55bf51d0eb5a05</p>
-    <p>SHA512:4b5dafa675246d1393d620c717af4ef7a3b0041ae87e3553d7e00e5b99d3cb065a21a467299b403930c06afa4d8fb069bfd412eed7e9b81d0bd81dcd63d2a90c</p>
+    <template #header>Choose file</template>
+    <el-button type="primary" plain @click="ten">10MB</el-button>
+    <el-button type="success" plain @click="one">100MB</el-button>
+    <el-button type="danger" plain @click="three">300MB</el-button>
+    <p>The format ends with .bin to prevent accidental operation, although the structure of these files is binary 0.</p>
+    <p>Because I am relatively poor, the server's hard drive is not very large. 300MB is considered a large file. Please use it sparingly. Thank you.</p>
   </el-card>
   <el-divider />
-  <el-card style="width: 100%" shadow="hover">
-    <template #header>Server information</template>
-    <p>Temporarily unavailable...</p>
+   <h2>iperf3</h2>
+  <p>I recommend you use iperf3 to test the speed. It is easy to use.</p>
+    <el-card style="width: 100%" shadow="hover">
+    <p :plain="true" @click="iperf">iperf3 -c [IP/Domain] -R (Click to copy.)</p>
+    <p>Meaning: iperf3 -client [speed test address] -Reverse (without -R, test upload)</p>
   </el-card>
+   <el-divider />
+  <el-card style="width: 100%" shadow="hover">
+    <template #header>About</template>
+    <p>Currently, all MoKanove servers support IPV4/V6 dual-stack networks and are equipped with at least 1Gbps bandwidth and Ubuntu Server system.</p>
+    <p @click="dn">Click on this line of text to view the code of this page, which is open source.</p>
+    <p>Copyright 2025 MoKanove All rights reserved.</p>
+    </el-card>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+
 const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 const th = () => {
-  window.open('./')
+  window.location.href = '/';
 }
 const pa = () => {
-  window.open('https://867678.xyz')
+  window.location.href = 'https://867678.xyz';
+}
+const ten = () => {
+  window.location.href = '10.bin';
+   ElMessage({
+    message: 'Downloading...',
+    type: 'primary',
+    plain: true,
+  })
+}
+const one = () => {
+  window.location.href = '100.bin';
+  ElMessage({
+    message: 'Downloading...',
+    type: 'primary',
+    plain: true,
+  })
+}
+const three = () => {
+  window.location.href = '300.bin';
+  ElMessage({
+    message: 'Downloading...',
+    type: 'primary',
+    plain: true,
+  })
+}
+const iperf = () => {
+ ElMessage({
+    message: 'Copy successful.',
+    type: 'primary',
+    plain: true,
+  })
+}
+const dn = () => {
+   window.open('https://github.com/mokanove/867678.xyz/tree/dn', '_blank')
 }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
-import './assets/home.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
 
