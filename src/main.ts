2f11af0a75594cc5b3ad00e48fa47ac6eb55ee4e
MoKanove <mpjy000@gmail.com>
2025-08-18 16:22:16 +0800

    empty

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './assets/home.css'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
-import router from './router/index'
-
-createApp(App).use(router).mount('#app')
-
