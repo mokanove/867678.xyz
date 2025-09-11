6e80ec843bd5acd709fe56a10068e3506232c461
Mo Kanove <mpjy000@gmail.com>
2025-09-11 16:35:00 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,11 +2,7 @@
 echo "this command by speedtest offical website:https://www.speedtest.net/apps/cli"
 echo "The author is not responsible for any consequences of using this code."
 read -p "please press enter start install." a
-echo "installing speedtest."
-sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
-sudo apt-get update
 sudo apt-get install wget -y
-sudo apt-get remove speedtest speedtest-cli -y
 wget https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
 tar -zxvf ookla-speedtest-1.2.0-linux-x86_64.tgz
 rm speedtest.md ookla-speedtest-1.2.0-linux-x86_64.tgz
